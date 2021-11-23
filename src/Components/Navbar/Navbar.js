265c50c55e88db376a542e8122a31338eef4916e
soulaymanegharroudi <soulaymane.gharroudi@student.vinci.be>
2021-11-23 22:50:49 +0100

    navbar finie

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -14,32 +14,15 @@ import { Navbar as BootstrapNavbar} from "bootstrap";
 const Navbar = () => {
   const navbarWrapper = document.querySelector("#navbarWrapper");
   let navbar = `
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Add your brand here</a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#" data-uri="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/new">New Page</a>
-              </li>                        
-            </ul>
-          </div>
+    <nav>
+        <a id="logo" href="#top">AJOUTER LOGO ICI</a> <!-- si on clique sur le logo on remonte en haut de la page -->
+        <div id="menu">
+          <button class="menuButton" id="homeButton">Home</button>
+          <button class="menuButton" id="tendanceButton">Top kwicks</button>
+          <button class="menuButton" id="messagesButton">Messages</button>
+          <button class="menuButton" id="settingsButton">Paramètres</button>
         </div>
-      </nav>
+    </nav>
   `;  
   navbarWrapper.innerHTML = navbar;
 };
--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -3,8 +3,8 @@
  */
 
 const HomePage = () => { 
-  const pageDiv = document.querySelector("#page");
-  pageDiv.innerHTML = "Deal with the content of your HomePage";
+  // const pageDiv = document.querySelector("#page");
+  // pageDiv.innerHTML = "";
 };
 
 export default HomePage;
