7fd2a664ac0ab789e2a807469659685c2b4e07a1
antoine <pirlotantoine2@gmail.com>
2021-12-14 20:46:16 +0100

    Refactor the refresh member table function is in utils directory

--- src/Components/Pages/AdminPage.js
+++ src/Components/Pages/AdminPage.js
@@ -1,3 +1,4 @@
+import Tables from "../../utils/tables";
 const userToken = JSON.parse(window.localStorage.getItem("user")).token;
 const getRequest = {
     method: "GET",
@@ -114,7 +115,7 @@ async function showMembersGestion() {
     adminTable.innerHTML = membersGestionHtml;
 
     try {
-        await refreshTable();
+        await Tables.refreshMembersTable();
         const forms = document.querySelectorAll("#membersGestionForm");
         forms.forEach((form) => {
             form.addEventListener("submit", (e) => {
@@ -155,52 +156,10 @@ async function removeUser(id_user) {
         const response = await fetch(`/api/users/${id_user}`, deleteRequest);
         if (!response.ok)
             throw new Error("fetch error : " + responsePosts.status + " : " + responsePosts.statusText);
-        await refreshTable()
+        await Tables.refreshMembersTable();
     } catch (e) {
         console.error(e);
     }
-}
-
-async function refreshTable() {
-    const response = await fetch("/api/users/all", getRequest);
-
-    if (!response.ok)
-        throw new Error("AdminPage::error: fetch error: fetch error : " + response.status + " : " + response.statusText);
-    const users = await response.json();
-    const tableTbody = document.getElementById("membersGestionTbody");
-    tableTbody.innerHTML = "";
-    users.forEach((user) => {
-        if (user.is_active)
-            var memberStatus = "Deactivate";
-        else
-            var memberStatus = "Activate";
-        if (user.is_admin)
-            var memberType = "Member";
-        else
-            var memberType = "Admin";
-
-            tableTbody.innerHTML += `
-            <tr>
-                <td>${user.id_user}</td>
-                <td>${user.forename}</td>   
-                <td>${user.lastname}</td>    
-                <td>${user.email}</td>
-                <td>${user.username}</td>
-                <td>${user.image}</td>
-                <td>${user.is_active}</td>
-                <td>${user.is_admin}</td>
-                <td>${user.biography}</td>
-                <td>${user.date_creation}</td>
-                <td>
-                    <form id="membersGestionForm">
-                        <input id="id_user" type="hidden" value="${user.id_user}">
-                        <input id="memberStatus" type="submit" value="${memberStatus}">
-                        <input id="memberType" type="submit" value="${memberType}">
-                    </form>
-                </td>
-            </tr>
-        `;
-    });
 }
 
 export default AdminPage;
