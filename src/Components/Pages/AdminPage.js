9faf30c2bbe032dc0c0f9fb27e50ade3ba2eae74
antoine <pirlotantoine2@gmail.com>
2021-12-14 20:39:38 +0100

    Add the possibility to deactivate user

--- src/Components/Pages/AdminPage.js
+++ src/Components/Pages/AdminPage.js
@@ -1,3 +1,11 @@
+const userToken = JSON.parse(window.localStorage.getItem("user")).token;
+const getRequest = {
+    method: "GET",
+    headers: {
+        "Authorisation": userToken
+    }
+};
+
 const adminPagehtml = `
     <div id="adminPage">
         <div id="adminButtons">
@@ -43,7 +51,7 @@ async function showPostsGestion() {
     const table = document.getElementById("postsGestionTable");
 
     try {
-        const response = await fetch("/api/posts/");
+        const response = await fetch("/api/posts/", getRequest);
         console.log(response)
         if (!response.ok)
             throw new Error("AdminPage::error: fetch error: fetch error : " + response.status + " : " + response.statusText);
@@ -79,7 +87,7 @@ async function showPostsGestion() {
 
 const membersGestionHtml = `
     <h3>Members Gestion</h3>
-    <form id="membersGetionForm"></form>
+ 
         <table id="membersGestionTable" class="table-bordered">
             <tr>
                 <th>User's Id</th>
@@ -94,55 +102,105 @@ const membersGestionHtml = `
                 <th>Creation Date</th>
                 <th>Modifications</th>
             </tr>
+            <tbody id="membersGestionTbody">
+            
+            </tbody>
         </table>
-    </form>
+ 
 `;
 
 async function showMembersGestion() {
     const adminTable = document.getElementById("adminTable");
     adminTable.innerHTML = membersGestionHtml;
-    const table = document.getElementById("membersGestionTable");
 
     try {
-        const response = await fetch("/api/users/");
-        console.log(response)
-        if (!response.ok)
-            throw new Error("AdminPage::error: fetch error: fetch error : " + response.status + " : " + response.statusText);
-        const users = await response.json();
-        console.log(users)
-        users.forEach((user) => {
-            if (user.is_active)
-                var memberStatus = "Activate";
-            else
-                var memberStatus = "Deactivate";
-            if (user.is_admin)
-                var memberType = "Member";
-            else
-                var memberType = "Admin";
+        await refreshTable();
+        const forms = document.querySelectorAll("#membersGestionForm");
+        forms.forEach((form) => {
+            form.addEventListener("submit", (e) => {
+                e.preventDefault();
+                let id_user;
+                let memberStatus;
+                let memberType;
+                form.querySelectorAll("input").forEach((input) => {
+                    if (input.id === "id_user")
+                        id_user = input.value;
+                    else if (input.id === "memberStatus") {
+                        memberStatus = input.value;
+                        input.addEventListener("click", (e) => {
+                            if (memberStatus === "Deactivate")
+                                console.log(id_user)
+                            removeUser(id_user);
+                        });
+                    } else {
+                        memberType = input.value;
 
-            table.innerHTML += `
-                <tr>
-                    <td>${user.id_user}</td>
-                    <td>${user.forename}</td>   
-                    <td>${user.lastname}</td>    
-                    <td>${user.email}</td>
-                    <td>${user.username}</td>
-                    <td>${user.image}</td>
-                    <td>${user.is_active}</td>
-                    <td>${user.is_admin}</td>
-                    <td>${user.biography}</td>
-                    <td>${user.date_creation}</td>
-                    <td>
-                        <input id="id_user" type="hidden" value="${user.id_user}">
-                        <input type="submit" value="${memberStatus}">
-                        <input type="submit" value="${memberType}">
-                    </td>
-                </tr>
-            `;
+                    }
+                });
+            });
         });
     } catch (e) {
         console.log(e.message);
     }
+}
+
+async function removeUser(id_user) {
+    const deleteRequest = {
+        method: "DELETE",
+        headers: {
+            "Authorisation": userToken
+        }
+    };
+    try {
+        const response = await fetch(`/api/users/${id_user}`, deleteRequest);
+        if (!response.ok)
+            throw new Error("fetch error : " + responsePosts.status + " : " + responsePosts.statusText);
+        await refreshTable()
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+async function refreshTable() {
+    const response = await fetch("/api/users/all", getRequest);
+
+    if (!response.ok)
+        throw new Error("AdminPage::error: fetch error: fetch error : " + response.status + " : " + response.statusText);
+    const users = await response.json();
+    const tableTbody = document.getElementById("membersGestionTbody");
+    tableTbody.innerHTML = "";
+    users.forEach((user) => {
+        if (user.is_active)
+            var memberStatus = "Deactivate";
+        else
+            var memberStatus = "Activate";
+        if (user.is_admin)
+            var memberType = "Member";
+        else
+            var memberType = "Admin";
+
+            tableTbody.innerHTML += `
+            <tr>
+                <td>${user.id_user}</td>
+                <td>${user.forename}</td>   
+                <td>${user.lastname}</td>    
+                <td>${user.email}</td>
+                <td>${user.username}</td>
+                <td>${user.image}</td>
+                <td>${user.is_active}</td>
+                <td>${user.is_admin}</td>
+                <td>${user.biography}</td>
+                <td>${user.date_creation}</td>
+                <td>
+                    <form id="membersGestionForm">
+                        <input id="id_user" type="hidden" value="${user.id_user}">
+                        <input id="memberStatus" type="submit" value="${memberStatus}">
+                        <input id="memberType" type="submit" value="${memberType}">
+                    </form>
+                </td>
+            </tr>
+        `;
+    });
 }
 
 export default AdminPage;
