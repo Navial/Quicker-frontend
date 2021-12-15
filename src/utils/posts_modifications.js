e8f04988b9325f01a7400ccfb227f7eeaae4bedd
antoine <pirlotantoine2@gmail.com>
2021-12-15 21:19:22 +0100

    Add - activatePost

--- src/utils/posts_modifications.js
+++ src/utils/posts_modifications.js
@@ -2,12 +2,32 @@ import Tables from "./tables";
 import user from "../models/User";
 
 let userToken;
+
+const putRequest = {
+    "method": "PUT",
+    headers: {
+        Authorization: userToken
+    }
+};
+
 const deleteRequest = {
     method: "DELETE",
     headers: {
         Authorization: userToken
     }
 };
+
+async function activatePost(id_post) {
+    loadToken();
+    try {
+        const response = await fetch(`/api/posts/activate/${id_post}`, putRequest);
+        if(!response.ok)
+            throw new Error("fetch error : " + response.status + " : " + response.statusText);
+        await Tables.refreshPostsTable();
+    } catch (e) {
+        console.error(e);
+    }
+}
 
 async function removePost(id_post){
     loadToken();
@@ -25,4 +45,4 @@ function loadToken() {
     userToken = JSON.parse(window.localStorage.getItem("user")).token;
 }
 
-export default {removePost};
+export default {activatePost, removePost};
