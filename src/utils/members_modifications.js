872c1ad5e7cc3f7059ec398c5adef42826b14e0d
antoine <pirlotantoine2@gmail.com>
2021-12-15 21:33:07 +0100

    Add - activateUser

--- src/utils/members_modifications.js
+++ src/utils/members_modifications.js
@@ -1,6 +1,12 @@
 import Tables from "./tables";
 
 let userToken;
+const putRequest = {
+    method: "PUT",
+    headers: {
+        Authorization: userToken
+    }
+}
 const deleteRequest = {
     method: "DELETE",
     headers: {
@@ -8,7 +14,16 @@ const deleteRequest = {
     }
 };
 
-
+async function activateUser(id_user) {
+    loadToken();
+    try {
+        const response = await fetch(`/api/users/activate/${id_user}`, putRequest);
+        if(!response.ok)
+            throw new Error("fetch error : " + response.status + " : " + response.statusText);
+    } catch (e) {
+        console.error(e);
+    }
+}
 
 async function deactivateUser(id_user) {
     loadToken();
