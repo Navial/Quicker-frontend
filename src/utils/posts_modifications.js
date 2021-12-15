698e650b1ce74ba8e34f56835b4206cce7d833f1
antoine <pirlotantoine2@gmail.com>
2021-12-15 20:18:14 +0100

    Add - removePost

---
+++ src/utils/posts_modifications.js
@@ -0,0 +1,23 @@
+import Tables from "./tables";
+
+let userToken;
+const deleteRequest = {
+    method: "DELETE",
+    headers: {
+        "Authrorisation": userToken
+    }
+}
+
+async function removePost(id_post){
+    userToken = JSON.parse(window.localStorage.getItem("user")).token;
+    try {
+        const response = await fetch(`/api/posts/${id_post}`, deleteRequest);
+        if (!response.ok)
+            throw new Error("fetch error : " + response.status + " : " + response.statusText);
+        await Tables.refreshPostsTable();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export default {removePost};
