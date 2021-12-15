85858cc5705c938da04db5c71a531c192ab347ed
antoine <pirlotantoine2@gmail.com>
2021-12-15 22:04:21 +0100

    Add - setAdmin

--- src/utils/members_modifications.js
+++ src/utils/members_modifications.js
@@ -14,6 +14,19 @@ const deleteRequest = {
     }
 };
 
+async function setAdmin(id_user) {
+    loadToken();
+    try {
+        const response = await fetch(`/api/users/setadmin/${id_user}`, putRequest);
+
+        if(!response.ok)
+            throw new Error("fetch error : " + response.status + " : " + response.statusText);
+        await Tables.refreshMembersTable();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
 async function activateUser(id_user) {
     loadToken();
     try {
@@ -42,4 +55,4 @@ function loadToken() {
     userToken = JSON.parse(window.localStorage.getItem("user")).token;
 }
 
-export default {activateUser, deactivateUser};
+export default {activateUser, deactivateUser, setAdmin};
