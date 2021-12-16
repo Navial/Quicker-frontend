e9c4ca19800d3e55008c5a12bf100da4a3ec0b4d
antoine <pirlotantoine2@gmail.com>
2021-12-16 21:56:46 +0100

    Refactor - rename

--- src/Components/Router/Router.js
+++ src/Components/Router/Router.js
@@ -3,7 +3,7 @@ import NewPage from "../Pages/NewPage";
 import RegisterPage from "../Pages/RegisterPage"
 import ProfilePage from "../Pages/ProfilePage";
 import TopKwicks from "../Pages/TopKwicks";
-import UserLikedPosts from "../Pages/UserLikedPosts";
+import UserLikesPage from "../Pages/UserLikesPage";
 import LoginPage from "../Pages/LoginPage";
 import AdminPage from "../Pages/AdminPage";
 import Logout from "../Pages/Logout";
@@ -15,7 +15,7 @@ const routes = {
   "/register": RegisterPage,
   "/profile" : ProfilePage,
   "/top_kwicks" : TopKwicks,
-  "/liked_posts": UserLikedPosts,
+  "/liked_posts": UserLikesPage,
   "/login": LoginPage,
   "/admin_page": AdminPage,
   "/logout": Logout
