bad747060a204a3c68f2b560ba05f87098b15077
antoine <pirlotantoine2@gmail.com>
2021-12-19 01:39:30 +0100

    Refactor - api request is in ApiModule

--- src/Components/Modules/GetPostsModule.js
+++ src/Components/Modules/GetPostsModule.js
@@ -53,15 +53,6 @@ function showPostsHtml(page, posts){
     });
 }
 
-/*
-* ██████╗░██████╗░        ███████╗██╗░░░██╗███╗░░██╗░█████╗░████████╗██╗░█████╗░███╗░░██╗
-* ██╔══██╗██╔══██╗        ██╔════╝██║░░░██║████╗░██║██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║
-* ██║░░██║██████╦╝        █████╗░░██║░░░██║██╔██╗██║██║░░╚═╝░░░██║░░░██║██║░░██║██╔██╗██║
-* ██║░░██║██╔══██╗        ██╔══╝░░██║░░░██║██║╚████║██║░░██╗░░░██║░░░██║██║░░██║██║╚████║
-* ██████╔╝██████╦╝        ██║░░░░░╚██████╔╝██║░╚███║╚█████╔╝░░░██║░░░██║╚█████╔╝██║░╚███║
-* ╚═════╝░╚═════╝░        ╚═╝░░░░░░╚═════╝░╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝
-* */
-
 async function GetPosts(page, profilePosts = null, isHomepage = false, isLikesPost = false) {
     try {
         const posts = await ApiModule.getPosts(profilePosts, isHomepage, isLikesPost);
@@ -70,8 +61,8 @@ async function GetPosts(page, profilePosts = null, isHomepage = false, isLikesPo
         // like listener
         const b = document.querySelectorAll('svg');
         [].slice.call(b).forEach(function (el) {
-            el.onclick = sendLike.bind(this, el);
-            isLiked(el.id);
+            el.onclick = ApiModule.sendLike.bind(this, el);
+            ApiModule.isLiked(el.id);
         });
 
         // remove post listener
@@ -83,74 +74,6 @@ async function GetPosts(page, profilePosts = null, isHomepage = false, isLikesPo
         });
     } catch (e){
         console.error(e);
-    }
-}
-
-async function isLiked(post){
-    const user = load_user.loadUser();
-    if (!post) return;
-
-    let request = {
-        method: "GET",
-        headers: {
-            "Authorization": user.token
-        }
-    };
-    try {
-        const reponseLikes = await fetch(`/api/likes/exist/` + user.id_user + `/` + post, request);
-        if (!reponseLikes.ok) {
-            throw new Error(
-                "fetch error : " + reponseLikes.status + " : " + reponseLikes.statusText
-            );
-        }
-
-        if (reponseLikes.status.toString().slice(-1) === "1") {
-            document.getElementsByClassName("likeButton").namedItem(post).innerHTML = `<path fill="currentColor" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path>`;
-        }
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-async function sendLike(post){
-    const user = load_user.loadUser();
-    const request = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": user.token
-        },
-        body: JSON.stringify(
-            {
-                id_user: user.id_user,
-                id_post: post.id
-            }
-        ),
-    };
-    try {
-        const responsePosts = await fetch(`/api/likes/toggle`, request);
-        if (!responsePosts.ok) {
-            throw new Error(
-                "fetch error : " + responsePosts.status + " : " + responsePosts.statusText
-            );
-        }
-
-        const likeCounter = document.getElementById("counter" + post.id);
-        const likeCounterInt = parseInt(likeCounter.innerHTML);
-        if (responsePosts.status === 201) {
-            likeCounter.innerHTML = likeCounterInt + 1;
-            document.getElementsByClassName("likeButton").namedItem(post.id).innerHTML = `<path fill="currentColor" d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path>`;
-        } else {
-            if (location.pathname !== "/liked_posts") {
-                likeCounter.innerHTML = likeCounterInt - 1;
-            } else {
-                likeCounter.parentNode.hidden = true;
-            }
-            document.getElementsByClassName("likeButton").namedItem(post.id).innerHTML = `<path fill="currentColor" d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"></path>`;
-        }
-
-    } catch (e) {
-        console.log(e);
     }
 }
 
