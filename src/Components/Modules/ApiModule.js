c36b2cdbd67d5175f390d0bf1e913506023dc8a0
antoine <pirlotantoine2@gmail.com>
2021-12-19 01:32:44 +0100

    Refactor - api request is in ApiModule

--- src/Components/Modules/ApiModule.js
+++ src/Components/Modules/ApiModule.js
@@ -14,8 +14,51 @@ async function sendMessage(body) {
         }
     };
     const response = await fetch("/api/messages/", request);
-    if(!response.ok)
+    if (!response.ok)
         throw new Error("fetch error : " + response.status + " : " + response.statusText);
 }
 
-export default {sendMessage};
+async function getPosts(profilePosts = null, isHomepage = false, isLikesPost = false) {
+    const user = load_user.loadUser();
+    let request = {
+        method: "GET",
+        headers: {
+            "Authorization": user.token
+        }
+    };
+
+    let responsePosts = await fetch(`/api/posts/allPostWithLikesAndUser/` + profilePosts, request);
+    if (isHomepage) {
+        request = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": user.token
+            },
+            body: JSON.stringify(
+                {
+                    id_user: user.id_user,
+                }
+            ),
+        };
+
+        responsePosts = await fetch(`/api/posts/homepage`, request);
+    } else if (isLikesPost) {
+        request = {
+            "method": "GET",
+            headers: {
+                Authorization: user.token
+            }
+        };
+        responsePosts = await fetch(`/api/posts/postsLiked/${user.id_user}`, request);
+    }
+
+    if (!responsePosts.ok) {
+        throw new Error(
+            "fetch error : " + responsePosts.status + " : " + responsePosts.statusText
+        );
+    }
+    return await responsePosts.json();
+}
+
+export default {sendMessage, getPosts};
--- src/Components/Modules/GetPostsModule.js
+++ src/Components/Modules/GetPostsModule.js
@@ -1,5 +1,6 @@
 import Posts_modifications from "../../utils/posts_modifications";
 import load_user from "../../utils/load_user";
+import ApiModule from "./ApiModule";
 
 function showPostsHtml(page, posts){
     const user = load_user.loadUser();
@@ -62,47 +63,8 @@ function showPostsHtml(page, posts){
 * */
 
 async function GetPosts(page, profilePosts = null, isHomepage = false, isLikesPost = false) {
-    const user = load_user.loadUser();
-    let request = {
-        method: "GET",
-        headers: {
-            "Authorization": user.token
-        }
-    };
-
-    let responsePosts= await fetch(`/api/posts/allPostWithLikesAndUser/` + profilePosts, request);
     try {
-        if (isHomepage) {
-            request = {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": user.token
-                },
-                body: JSON.stringify(
-                    {
-                        id_user: user.id_user,
-                    }
-                ),
-            };
-
-            responsePosts  = await fetch(`/api/posts/homepage`, request);
-        } else if (isLikesPost) {
-            request = {
-                "method": "GET",
-                headers: {
-                    Authorization: user.token
-                }
-            };
-            responsePosts = await fetch(`/api/posts/postsLiked/${user.id_user}`, request);
-        }
-
-        if (!responsePosts.ok) {
-            throw new Error(
-                "fetch error : " + responsePosts.status + " : " + responsePosts.statusText
-            );
-        }
-        const posts = await responsePosts.json();
+        const posts = await ApiModule.getPosts(profilePosts, isHomepage, isLikesPost);
         showPostsHtml(page, posts);
 
         // like listener
@@ -119,7 +81,6 @@ async function GetPosts(page, profilePosts = null, isHomepage = false, isLikesPo
                 e.target.parentNode.parentNode.parentNode.hidden = true;
             }
         });
-
     } catch (e){
         console.error(e);
     }
